3c75de36d888a295ecc95082a6616270d750c2a0
tuazucar13min-dev <tuazucar13min@gmail.com>
2025-10-31 22:34:01 -0500

    SEMANA 12

---
+++ CLASEGENERICA/src/main/java/com/mycompany/clasegenerica/CLASEGENERICA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clasegenerica;
+
+/**
+ *
+ * @author CIELO
+ */
+public class CLASEGENERICA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
